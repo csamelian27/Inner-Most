667af03016c9dd06eee5d2f41ef7dfef2351ab5e
Elizabeth Le <elizabethlemk@yahoo.com>
2019-04-30 16:58:52 -0400

    plx

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Container, Header, Icon, Form } from "semantic-ui-react";
-import UserChart from './UserChart';
+import UserChart from "./UserChart";
+require("dotenv").config();
 
 class Home extends React.Component {
   state = {
@@ -14,35 +15,33 @@ class Home extends React.Component {
   };
 
   handleSubmit = () => {
-    console.log('bob', this.state.username);
-      fetch(`https://cors-anywhere.herokuapp.com/https://api.twitter.com/1.1/statuses/user_timeline/${this.state.username}.json?count=2`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer AAAAAAAAAAAAAAAAAAAAAKik9wAAAAAAeEy8mNdChB3cu0SaakLzTJvOiJk%3Dl39cr6lRLidFjMwu4d5LVWHZrw312YT4k08OpCrApjw8NERmvG"
-        }
-      }).then(resp=>resp.json()).then(tweets => {
+    console.log("bob", this.state.username);
+    fetch(`${process.env.TWITTER_URL}/${this.state.username}.json?count=2`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.TWITTER_API_KEY}`
+      }
+    })
+      .then(resp => resp.json())
+      .then(tweets => {
         this.setState({
           tweets: tweets
-        })
-        tweets.forEach(tweet => this.checkEmotion(tweet))
-      })
+        });
+        tweets.forEach(tweet => this.checkEmotion(tweet));
+      });
   };
 
   checkEmotion = tweet => {
     var unirest = require("unirest");
     unirest
-      .post("https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/")
+      .post(`${process.env.EMOTION_URL}`)
       .header("X-RapidAPI-Host", "twinword-emotion-analysis-v1.p.rapidapi.com")
-      .header(
-        "X-RapidAPI-Key",
-        "16cc5bd8dcmsh6907259db58b42cp1abd80jsn031467cd6f42"
-      )
+      .header("X-RapidAPI-Key", `${process.env.EMOTION_API_KEY}`)
       .header("Content-Type", "application/x-www-form-urlencoded")
       .send(`text=${tweet}`)
       .end(emotion => {
         console.log(emotion, tweet);
-        this.setState({emotions: [...this.state.emotions, emotion.body]})
+        this.setState({ emotions: [...this.state.emotions, emotion.body] });
       });
   };
 
@@ -55,7 +54,7 @@ class Home extends React.Component {
         </Header>
         <Form onSubmit={this.handleSubmit}>
           <Form.Input
-          disabled={!this.props.signedIn.isSignedIn}
+            disabled={!this.props.signedIn.isSignedIn}
             id="twitter-input"
             value={this.state.username}
             onChange={this.handleChange}
@@ -63,7 +62,13 @@ class Home extends React.Component {
             style={{ maxWidth: "200px" }}
           />
           <Button inverted> Submit </Button>
-          {this.state.emotions.length === this.state.tweets.length && this.state.emotions.length > 0 ? <UserChart emotions={this.state.emotions} tweets={this.state.tweets} /> : null}
+          {this.state.emotions.length === this.state.tweets.length &&
+          this.state.emotions.length > 0 ? (
+            <UserChart
+              emotions={this.state.emotions}
+              tweets={this.state.tweets}
+            />
+          ) : null}
         </Form>
       </Container>
     );
